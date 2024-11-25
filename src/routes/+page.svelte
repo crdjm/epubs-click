b8b09387379d3bb5f834e0374e50b0573d11e257
David Metcalfe <crdjm1@gmail.com>
2024-11-25 13:47:22 -0600

    Simple UI change to test vercel update process

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex h-screen flex-col items-center justify-center">
 	<h1
-		class="font-manrope text-xl font-extrabold leading-snug text-indigo-600 [text-shadow:_0_2px_4px_rgb(99_102_241_/_0.8)] md:text-2xl"
+		class="font-manrope text-xl font-extrabold leading-snug text-blue-600 [text-shadow:_0_2px_4px_rgb(99_102_241_/_0.8)] md:text-2xl"
 	>
 		This is a placeholder page for epubs.click
 	</h1>
