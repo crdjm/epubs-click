21d89ff14da1ef4738821665d191e17163064a45
David Metcalfe <crdjm1@gmail.com>
2024-11-25 12:55:03 -0600

    Update placeholder text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,14 @@
 	import { Button } from '$lib/components/ui/button';
 </script>
 
-<h1
-	class="font-manrope flex h-screen items-center justify-center text-xl font-extrabold leading-snug text-indigo-600 [text-shadow:_0_2px_4px_rgb(99_102_241_/_0.8)] md:text-2xl"
->
-	This is a placeholder page for epubs.click
-</h1>
+<div class="flex h-screen flex-col items-center justify-center">
+	<h1
+		class="font-manrope text-xl font-extrabold leading-snug text-indigo-600 [text-shadow:_0_2px_4px_rgb(99_102_241_/_0.8)] md:text-2xl"
+	>
+		This is a placeholder page for epubs.click
+	</h1>
+
+	<p>Pleash check back later.</p>
+</div>
 
 <!-- <Button variant="outline">Button</Button> -->
