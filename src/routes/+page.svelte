b6190e603fce5e873f6e1b8df17c7c6be0ba9cba
David Metcalfe <crdjm1@gmail.com>
2024-11-26 14:55:07 -0600

    Added general info to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,58 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
+	// import { Button } from '$lib/components/ui/button';
 </script>
 
-<div class="flex h-screen flex-col items-center justify-center">
+<div class="flex h-screen w-full flex-col items-center bg-gray-500 font-serif">
 	<h1
-		class="font-manrope text-xl font-extrabold leading-snug text-blue-600 [text-shadow:_0_2px_4px_rgb(99_102_241_/_0.8)] md:text-2xl"
+		class="inline-block bg-gradient-to-r from-blue-600 to-blue-300 bg-clip-text py-4 text-8xl font-bold text-transparent drop-shadow-[0_2.2px_1.2px_rgba(0,0,0,0.4)]"
 	>
-		This is a placeholder page for epubs.click
+		Epub.click
 	</h1>
 
-	<p>Pleash check back later.</p>
+	<h2 class="py-2 italic">Read and Modify EPUBs</h2>
+	<h2>David Metcalfe (crdjm1@gmail.com)</h2>
+
+	<p class="pt-8">
+		Epub.click will provide various resources for reading and modifying EPUBs, initially:
+	</p>
+	<ul
+		class="mt-5 list-inside list-disc rounded-lg border-2 border-solid border-gray-300 bg-gray-400 p-4"
+	>
+		<li class="pt-2">EpubHub, an npm package for reading, searching and updating epubs</li>
+		<li class="pt-2">
+			EpubHub-test, a test suite for EpubHub that may be used to build your own applications
+		</li>
+		<li class="pt-2">EpubHub-cli, a command line interface for EpubHub</li>
+		<li class="pt-2">EpubHub-gui, a graphical user interface for EpubHub</li>
+		<li class="pt-2">EpubHub-web (this site), a web application for EpubHub</li>
+	</ul>
+
+	<p class="pt-5">
+		EpubHub is available via npm today, and the source downloadable from <a
+			target="_blank"
+			class="text-blue-600 underline visited:text-blue-700 hover:text-blue-800"
+			href="https://github.com/crdjm/epubhub">GitHub</a
+		>. EpubHub-test is available via
+		<a
+			target="_blank"
+			class="text-blue-600 underline visited:text-blue-700 hover:text-blue-800"
+			href="https://github.com/crdjm/epubhub-test">GitHub</a
+		>.
+	</p>
+
+	<p class="pt-5">
+		EpubHub and the derived projects will utilise AI to improve the quality of the content when
+		appropriate.
+	</p>
+
+	<p class="pt-5">
+		All of the above is still <span class="italic">in development</span>. Please check back for
+		updates, or contact me at
+		<a
+			class="text-blue-600 underline visited:text-blue-700 hover:text-blue-800"
+			href="mailto:crdjm1@gmail.com?subject=EpubHub">crdjm1@gmail.com</a
+		> if you have any questions.
+	</p>
 </div>
 
 <!-- <Button variant="outline">Button</Button> -->
