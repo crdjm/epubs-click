35387e0b7fdd9e1b352ed4b84de1f0871b53c92c
David Metcalfe <crdjm1@gmail.com>
2024-11-26 18:20:15 -0600

    Improve render on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,30 +4,35 @@
 
 <div class="flex h-screen w-full flex-col items-center bg-gray-500 font-serif">
 	<h1
-		class="inline-block bg-gradient-to-r from-blue-600 to-blue-300 bg-clip-text py-4 text-8xl font-bold text-transparent drop-shadow-[0_2.2px_1.2px_rgba(0,0,0,0.4)]"
+		class="inline-block bg-gradient-to-r from-blue-600 to-blue-300 bg-clip-text py-2 text-5xl font-bold text-transparent drop-shadow-[0_2.2px_1.2px_rgba(0,0,0,0.4)]"
 	>
 		Epubs.click
 	</h1>
 
 	<h2 class="py-2 italic">Read and Modify EPUBs</h2>
-	<h2>David Metcalfe (crdjm1@gmail.com)</h2>
+	<h2>
+		David Metcalfe (<a
+			class="text-blue-600 underline visited:text-blue-700 hover:text-blue-800"
+			href="mailto:crdjm1@gmail.com?subject=EpubHub">crdjm1@gmail.com</a
+		>)
+	</h2>
 
-	<p class="pt-8">
+	<p class="pt-2">
 		Epubs.click will provide various resources for reading and modifying EPUBs, initially:
 	</p>
 	<ul
-		class="mt-5 list-inside list-disc rounded-lg border-2 border-solid border-gray-300 bg-gray-400 p-4"
+		class="mt-2 list-inside list-disc rounded-lg border-2 border-solid border-gray-300 bg-gray-400 p-2"
 	>
-		<li class="pt-2">EpubHub, an npm package for reading, searching and updating epubs</li>
-		<li class="pt-2">
+		<li class="pt-1">EpubHub, an npm package for reading, searching and updating epubs</li>
+		<li class="pt-1">
 			EpubHub-test, a test suite for EpubHub that may be used to build your own applications
 		</li>
-		<li class="pt-2">EpubHub-cli, a command line interface for EpubHub</li>
-		<li class="pt-2">EpubHub-gui, a graphical user interface for EpubHub</li>
-		<li class="pt-2">EpubHub-web (this site), a web application for EpubHub</li>
+		<li class="pt-1">EpubHub-cli, a command line interface for EpubHub</li>
+		<li class="pt-1">EpubHub-gui, a desktop graphical user interface for EpubHub</li>
+		<li class="pt-1">EpubHub-web (this site), a web application for EpubHub</li>
 	</ul>
 
-	<p class="pt-5">
+	<p class="pt-2">
 		EpubHub is available via npm today, and the source downloadable from <a
 			target="_blank"
 			class="text-blue-600 underline visited:text-blue-700 hover:text-blue-800"
@@ -40,12 +45,12 @@
 		>.
 	</p>
 
-	<p class="pt-5">
+	<p class="pt-2">
 		EpubHub and the derived projects will utilise AI to improve the quality of the content when
 		appropriate.
 	</p>
 
-	<p class="pt-5">
+	<p class="pt-2">
 		All of the above is still <span class="italic">in development</span>. Please check back for
 		updates, or contact me at
 		<a
