3fb67d9e052400a49c80adec2b7aef733dca3fa6
David Metcalfe <crdjm1@gmail.com>
2024-12-03 13:41:45 -0600

    Add missing files from last build

---
+++ src/lib/components/app-sidebar.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	import Calendar from 'lucide-svelte/icons/calendar';
+	import House from 'lucide-svelte/icons/house';
+	import Inbox from 'lucide-svelte/icons/inbox';
+	import Search from 'lucide-svelte/icons/search';
+	import Settings from 'lucide-svelte/icons/settings';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+
+	// Menu items.
+	const items = [
+		{
+			title: 'Home',
+			url: '#',
+			icon: House
+		},
+		{
+			title: 'EpubHub',
+			url: '#',
+			icon: Inbox
+		},
+		{
+			title: 'EpubHub-test',
+			url: '#',
+			icon: Calendar
+		},
+		{
+			title: 'EpubHub-cli',
+			url: '#',
+			icon: Search
+		},
+		{
+			title: 'EpubHub-gui',
+			url: '#',
+			icon: Settings
+		},
+		{
+			title: 'EpubHub-web',
+			url: '#',
+			icon: Settings
+		}
+	];
+</script>
+
+<Sidebar.Root collapsible="icon">
+	<Sidebar.Content>
+		<Sidebar.Group>
+			<Sidebar.GroupLabel>Epubs.click</Sidebar.GroupLabel>
+			<Sidebar.GroupContent>
+				<Sidebar.Menu>
+					{#each items as item (item.title)}
+						<Sidebar.MenuItem>
+							<Sidebar.MenuButton>
+								{#snippet child({ props })}
+									<a href={item.url} {...props}>
+										<item.icon />
+										<span>{item.title}</span>
+									</a>
+								{/snippet}
+							</Sidebar.MenuButton>
+						</Sidebar.MenuItem>
+					{/each}
+				</Sidebar.Menu>
+			</Sidebar.GroupContent>
+		</Sidebar.Group>
+	</Sidebar.Content>
+</Sidebar.Root>
